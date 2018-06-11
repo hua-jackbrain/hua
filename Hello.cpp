962a1a2297f2ba6a8447802f1439dee87e888c02
hua-jackbrain <3104618774@qq.com>
2018-06-11 20:47:02 +0800

    add all

---
+++ Hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a = 500;
+    cout<<a;
+    return 0;
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("Hello World");
+let one = '500'
+console.log(one)
+console.log("Hello");
+
